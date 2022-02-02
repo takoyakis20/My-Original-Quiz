063ef5811dbbdd8128912c47d59e24b25cf5a7bd
takoyakis20 <37450315+takoyakis20@users.noreply.github.com>
2022-02-02 18:36:11 +0900

    Initial commit

---
+++ quiz1.js
@@ -0,0 +1,13 @@
+function ans(t) {
+    if (t == "南河内郡太子町")    { 
+        alert("正解! 大和郡山市は奈良の北部で奈良高専がある。 吉野郡は奈良県の南東にあり、桜の名所。三重との県境でもある。");
+    }
+    else
+    {
+        alert("不正解!");
+    }
+}
+
+function reload() {
+    document.createElement("h1").innerText = "aaaaaa";
+}
